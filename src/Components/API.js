9d3b64fdca41c99e760f7d193b5d24e4028050ca
Oustoir <houssop4@gmail.com>
2021-07-01 14:06:35 +0200

    VERSION FINAL FINALFANTASY

--- src/Components/API.js
+++ src/Components/API.js
@@ -1,21 +1,42 @@
+
 import { useState, useEffect } from 'react';
 import '../styles/API.css'
 import axios from 'axios'
 
-let url = "https://newsapi.org/v2/top-headlines?country=fr&apiKey=f935ef97b74e4d9db8cd9f7425d7663d"
+
+
+
+
 
 function API() {
   const [product, setProduct] = useState()
+  const [url, seturl] = useState(`https://newsapi.org/v2/top-headlines?country=fr&apiKey=f935ef97b74e4d9db8cd9f7425d7663d`)
+
+  function btn(cat){
+    seturl(`https://newsapi.org/v2/top-headlines?country=fr&${cat}apiKey=f935ef97b74e4d9db8cd9f7425d7663d`)
+    };
+
 
   useEffect(() => {
+    
     axios.get(url)
       .then(res => {
         setProduct(res.data.articles)
       })
-  }, [])
+  }, [url])
   
   return (
-    <div className='Main'>
+    <div>
+      <div className='divbtn'>
+          <button onClick={() => {btn('')}}>ALL</button>
+          <button onClick={() => {btn('category=business&')}}>business</button>
+          <button onClick={() => {btn('category=entertainment&')}}>entertainment</button>
+          <button onClick={() => {btn('category=health&')}}>health</button>
+          <button onClick={() => {btn('category=science&')}}>science</button>
+          <button onClick={() => {btn('category=sports&')}}>sports</button>
+          <button onClick={() => {btn('category=technology&')}}>technology</button>
+      </div>
+      <div className='Main'>
         {product && product.map((a) => 
         
           <div className='Article' key={a.publishedAt}>
@@ -30,10 +51,12 @@ function API() {
             </div>
             </a>
           </div>
-        
         )}
     </div>
+  </div>  
   );
 }
+
+
 
 export default API;
