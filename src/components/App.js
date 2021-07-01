d03998f18672bc9cd363db118947115cd3c1a2cc
PGredat <gredat.pierre@gmail.com>
2021-07-01 09:18:25 +0200

    footer

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+
+
+function App(){
+    return (
+        <div>
+            <Footer/>
+        </div>
+    )
+
+}
+
+export default App
---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import '../styles/Footer.css'
+import Email from "../assets/Email_logo.svg"
+import Acc from "../assets/Acc_logo.svg"
+import Article from "../assets/Article_logo.svg"
+import Settings from "../assets/Settings_logo.svg"
+
+function Footer({ children }) {
+	return <div className='footer'>{children}
+                
+                    
+                    <div class='footer-img'><img src={Article} /></div>
+                    <div class='footer-img'><img src={Email} /></div>
+                    <p> Coordonnées : adresse.random@gmail.com</p>
+                    <div class='footer-img'><img src={Settings} /></div>
+                    <div class='footer-img'><img src={Acc} /></div>
+               
+                
+            </div>
+}
+
+export default Footer
